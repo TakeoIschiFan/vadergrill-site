feb2a8d52baff7ba9024983d6c59a2e38f1b4f15
Tom Burgelman <tom.burgelman@me.com>
2022-11-12 14:45:08 +0100

    added custom error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export function load({ error }) {
+    return {
+      props: { message: error.message },
+    };
+  }
+</script>
+
+<script>
+  export let message;
+</script>
+
+<div id="error">
+  <h2>Oopsies</h2>
+  <p>{message}</p>
+  <p>head back home</p>
+</div>
+
+<style>
+  #error {
+    text-align: center;
+    margin-bottom: 60px;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-3xl font-bold underline">Welkom bij vader grill</h1>
 <p>website in opbouw</p>
-<p style="background-image: url('vadergrill.svg');"/> 
+<p style="background-image: url('vadergrill.svg');" />
