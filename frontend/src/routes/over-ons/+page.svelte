2a6925e1c4d017c58ea8d091e9ba723904661859
Tom Burgelman <tom.burgelman@me.com>
2022-11-12 16:50:49 +0100

    grid added to over ons

--- frontend/src/routes/over-ons/+page.svelte
+++ frontend/src/routes/over-ons/+page.svelte
@@ -1,24 +1,70 @@
-<h1 class="text-3xl font-bold font-mono Underline">Praesidium</h1>
+<h1 class="text-3xl font-bold font-mono">Praesidium</h1>
 
-<div class="flex p-6 font-mono">
-  <div
-    class="flex w-48 mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
-  >
-    <img
-      src="/testfoto.jpg"
-      alt=""
-      class="relative z-10 inset-0 w-full h-full object-cover rounded-lg"
-      loading="lazy"
-    />
-  </div>
-  <div class="flex pl-6">
+<div id="wrapper1">
+  <div class="flex">
     <div
-      class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
     >
-      <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
-        Praeses
-      </h1>
-      <div class="relative text-lg text-white">Andreas Weiermann</div>
+      <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 w-full h-full object-cover rounded-lg"
+        loading="lazy"
+      />
+    </div>
+    <div class="flex pl-6">
+      <div
+        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+      >
+        <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
+          Praeses
+        </h1>
+        <div class="relative text-lg text-white">Andreas Weiermann</div>
+      </div>
+    </div>
+  </div>
+  <div class="flex">
+    <div
+      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+    >
+      <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 w-full h-full object-cover rounded-lg"
+        loading="lazy"
+      />
+    </div>
+    <div class="flex pl-6">
+      <div
+        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+      >
+        <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
+          Praeses
+        </h1>
+        <div class="relative text-lg text-white">Andreas Weiermann</div>
+      </div>
+    </div>
+  </div>
+  <div class="flex">
+    <div
+      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+    >
+      <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 w-full h-full object-cover rounded-lg"
+        loading="lazy"
+      />
+    </div>
+    <div class="flex pl-6">
+      <div
+        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+      >
+        <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
+          Praeses
+        </h1>
+        <div class="relative text-lg text-white">Andreas Weiermann</div>
+      </div>
     </div>
   </div>
 </div>
