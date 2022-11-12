4bb43f0c1b65a0034ea1c497a3f12a2d4c800715
Tom Burgelman <tom.burgelman@me.com>
2022-11-12 16:12:25 +0100

    Over ons update test

--- frontend/src/routes/over-ons/+page.svelte
+++ frontend/src/routes/over-ons/+page.svelte
@@ -1,1 +1,24 @@
-Wie zijn wij?
+<h1 class="text-3xl font-bold font-mono Underline">Praesidium</h1>
+
+<div class="flex p-6 font-mono">
+  <div
+    class="flex w-48 mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+  >
+    <img
+      src="/testfoto.jpg"
+      alt=""
+      class="relative z-10 inset-0 w-full h-full object-cover rounded-lg"
+      loading="lazy"
+    />
+  </div>
+  <div class="flex pl-6">
+    <div
+      class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+    >
+      <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
+        Praeses
+      </h1>
+      <div class="relative text-lg text-white">Andreas Weiermann</div>
+    </div>
+  </div>
+</div>
