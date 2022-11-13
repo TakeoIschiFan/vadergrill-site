829742fa70a714cf422f86d898d965243df926ae
Tom Burgelman <tom.burgelman@me.com>
2022-11-13 18:13:23 +0100

    navbar update

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,35 +1,82 @@
 <script>
-    import "../app.css";
+  import { src_url_equal, svg_element } from "svelte/internal";
+  import "../app.css";
+
+  export let open = false;
+  export let onClick = () => {
+    open = !open;
+  };
+  let current = 0;
 </script>
 
-
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
+<nav class="bg-black sticky">
+  <div class="px-8 mx-atuo">
+    <div class="flex justify-between">
+      <div>
         <a href="/" class="flex items-center">
-            <img src="/vadergrill.svg" class="mr-3 h-6 sm:h-9" alt="Vader Grill Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Vader Grill</span>
+          <img src="/vadergrill.svg" class="h-16" alt="" />
+          <span
+            class="font-bold self-center text-xl whitespace-nowrap py-2 px-3 hover:text-red-600"
+            >Vader Grill</span
+          >
         </a>
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-        </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                <a href="/" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                </li>
-                <li>
-                <a href="/over-ons" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Over Ons</a>
-                </li>
-                <li>
-                <a href="/evenementen" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Evenementen</a>
-                </li>
-                <li>
-                <a href="/login" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
-                </li>
-            </ul>
+      </div>
+      <button on:click={onClick} aria-expanded={open}>
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class:open
+          viewBox="0 0 100 100"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="5"
+          id="hamburger"
+        >
+          <path
+            class="top"
+            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+          />
+          <path class="middle" d="m 30,50 h 40" />
+          <path
+            class="bottom"
+            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+          />
+        </svg>
+      </button>
+      <div class="hidden md:flex items-center">
+        <div>
+          <a
+            href="/"
+            class="py-2 px-3 hover:bg-red-900 rounded font-bold"
+            class:current={current === 0}
+            on:click={() => (current = 1)}>Home</a
+          >
+          <a
+            href="/evenementen"
+            class="py-2 px-3 hover:bg-red-900 rounded font-bold"
+            class:current={current === 1}
+            on:click={() => (current = 1)}
+          >
+            Evenementen
+          </a>
+          <a
+            href="/over-ons"
+            class="py-2 px-3 hover:bg-red-900 rounded font-bold"
+            class:current={current === 2}
+            on:click={() => (current = 1)}
+          >
+            Over Ons
+          </a>
+          <a
+            href="/login"
+            class="py-2 px-3 bg-red-600 rounded hover:bg-red-900 font-bold"
+          >
+            Login</a
+          >
         </div>
+      </div>
     </div>
+  </div>
+  <div class="bg-red-600 mx-auto h-2" />
 </nav>
 
 <slot />
