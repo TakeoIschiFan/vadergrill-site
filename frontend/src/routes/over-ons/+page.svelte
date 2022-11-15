ae2e7ccee4b9dbe14447dc871adc8c619c017ebc
Tom.Burgelman <tom.burgelman@me.com>
2022-11-15 11:32:45 +0100

    rounded edges

--- frontend/src/routes/over-ons/+page.svelte
+++ frontend/src/routes/over-ons/+page.svelte
@@ -1,9 +1,10 @@
 <p class="text-3xl font-bold font-mono underline">Praesidium</p>
-
+<!-- Grid use for better layout-->
 <div id="wrapper1" class="relative top-12">
+  <!--Item 1-->
   <div class="flex mx-3 my-3">
     <div
-      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+      class="flex mb-10 relative z-10 before:absolute before:rounded-lg before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
     >
       <img
         src="/testfoto.jpg"
@@ -14,7 +15,7 @@
     </div>
     <div class="flex pl-6">
       <div
-        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+        class="relative flex flex-wrap items-baseline pb-6 before:rounded-lg before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
       >
         <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
           Praeses
@@ -25,7 +26,7 @@
   </div>
   <div class="flex mx-3 my-3">
     <div
-      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+      class="flex mb-10 relative z-10 before:absolute before:rounded-lg before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
     >
       <img
         src="/testfoto.jpg"
@@ -36,7 +37,7 @@
     </div>
     <div class="flex pl-6">
       <div
-        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+        class="relative flex flex-wrap items-baseline pb-6 before:rounded-lg before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
       >
         <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
           Praeses
@@ -47,7 +48,7 @@
   </div>
   <div class="flex mx-3 my-3">
     <div
-      class="flex mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
+      class="flex mb-10 relative z-10 before:absolute before:rounded-lg before:top-1 before:left-1 before:w-full before:h-full before:bg-red-600"
     >
       <img
         src="/testfoto.jpg"
@@ -58,7 +59,7 @@
     </div>
     <div class="flex pl-6">
       <div
-        class="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
+        class="relative flex flex-wrap items-baseline pb-6 before:rounded-lg before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6"
       >
         <h1 class="relative w-full flex mb-2 text-2xl font-semibold text-white">
           Praeses
