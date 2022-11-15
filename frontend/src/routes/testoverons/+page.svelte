15cea319e98032a0b6078fd8ed82a7a4042e4591
Tom.Burgelman <tom.burgelman@me.com>
2022-11-15 12:33:25 +0100

    idk

--- frontend/src/routes/testoverons/+page.svelte
+++ frontend/src/routes/testoverons/+page.svelte
@@ -1,6 +1,30 @@
 <h1 class="text-white text-3xl underline font-bold">Test</h1>
 
 <div id="wrapper" class="relative">
+    <div class="border-l-2 border-r-2 w-max h-max overonsimg">
+        <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 object-cover"
+        width="200" height="600"
+    />
+    </div>
+    <div class="border-l-2 border-r-2 w-max overonsimg">
+        <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 object-cover"
+        width="200" height="200"
+    />
+    </div>
+    <div class="border-l-2 border-r-2 w-max overonsimg">
+        <img
+        src="/testfoto.jpg"
+        alt=""
+        class="relative z-10 inset-0 object-cover"
+        width="200" height="200"
+    />
+    </div>
     <div class="border-l-2 border-r-2 w-max overonsimg">
         <img
         src="/testfoto.jpg"
@@ -38,7 +62,7 @@
 <style>
 #wrapper {
     display: grid;
-    gap: 1
+    grid-template-columns: repeat(12, minmax(240px, 1fr));
 }
 .overonsimg:hover {
     transform: scale(1.2);
