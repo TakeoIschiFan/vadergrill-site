8c6107bc1434116f938879fb119a13f96ebdb5cb
Tom.Burgelman <tom.burgelman@me.com>
2022-11-21 11:04:19 +0100

    dingen proberen + login gemaakt

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <header>
-  <img src="vadergrill.svg" id="headerlogo" height="180px" width="180px" />
+  <img src="vadergrill.svg" id="headerlogo" height="180px" width="180px"/>
   <h1>Welkom bij vader grill</h1>
 </header>
 <p class="m-4 min-h-full">website in opbouw</p>
