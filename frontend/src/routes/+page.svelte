1bd0cc7148dbaec5160519413618495d0df55ad7
Tom Burgelman <tom.burgelman@me.com>
2022-11-27 23:05:57 +0100

    curves

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,39 @@
 <header class="mb-20">
   <div id="headerbox">
-    <img src="vadergrill.svg" id="headerlogo" height="170px" width="170px" alt=""/>
-    <h1 class="text-3xl text-white font-bold" id="mainname">VADER GRILL</h1>
+    <img
+      src="vadergrill.svg"
+      id="headerlogo"
+      height="170px"
+      width="170px"
+      alt=""
+    />
+    <h1 class="text-3xl font-bold" id="mainname">VADER GRILL</h1>
   </div>
-    <div class="lint">
-    <div class="bg-black -z-10 lint-zwart w-full"/>
-    <div class="bg-red-600 -z-20 lint-rood w-full"/>
+  <div class="lint">
+    <div class="bg-black -z-10 lint-zwart w-full" />
+    <div class="bg-red-600 -z-20 lint-rood w-full" />
   </div>
 </header>
-<h1 class="mx-4 my-8 text-3xl font-bold underline text-center">Welkom bij Vader Grill!</h1>
+<h1 class="mx-4 my-8 text-3xl font-bold underline text-center">
+  Welkom bij Vader Grill!
+</h1>
 <p class="m-4 text-center">
- De Studentenvereniging voor studenten die houden van BBQ!
+  De Studentenvereniging voor studenten die houden van BBQ!
 </p>
+
+<section class="bg-black">
+  <h1 class="text-3xl font-bold ">Over Ons</h1>
+  <div class="custom-shape-divider-top-1669586482">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+        class="shape-fill"
+      />
+    </svg>
+  </div>
+</section>
