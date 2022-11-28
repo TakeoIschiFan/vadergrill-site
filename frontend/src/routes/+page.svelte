807e79a1608b00710800d4c5105febd70613da76
Tom.Burgelman <tom.burgelman@me.com>
2022-11-28 23:44:27 +0100

    text box?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,21 +12,24 @@
   <div id="lint">
     <div class="bg-black -z-10 lint-zwart w-full" />
     <div class="bg-red-600 -z-20 lint-rood w-full" />
-    </div>
+  </div>
 </header>
+<section class="bg-black m-8 rounded-3xl text-center">
 <h1 class="mx-4 my-8 text-3xl font-bold underline text-center">
   Welkom bij Vader Grill!
 </h1>
 <p class="m-4 text-center">
   De Studentenvereniging voor studenten die houden van BBQ!
 </p>
+</section>
 
-<section class="bg-black m-8 rounded-3xl text-center">
+<section class="bg-red-900 m-8 rounded-3xl text-center">
   <h1 class="text-3xl font-bold underline m-4">Over Ons</h1>
   <p>
-    Wij zijn een Studentenvereniging die zich inzet voor mensen die houden van bbq. <br> Opgericht in het jaar 2022 en nog steeds levend!
+    Wij zijn een Studentenvereniging die zich inzet voor mensen die houden van
+    bbq. <br /> Opgericht in het jaar 2022 en nog steeds levend!
   </p>
-<!--
+  <!--
     <div class="custom-shape-divider-top-1669586482">
     <svg
       data-name="Layer 1"
