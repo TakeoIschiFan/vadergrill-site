da7b8522f052e63187b5dd200461af8c0a87cca6
Tom.Burgelman <tom.burgelman@me.com>
2022-11-29 23:12:44 +0100

    deleted waves

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
   </div>
 </header>
 <!--Each section is a different block on the page-->
-<section class="bg-black m-8 rounded-3xl text-center">
+<section class="m-8 rounded-3xl text-center">
 <h1 class="mx-4 my-8 text-3xl font-bold underline text-center">
   Welkom bij Vader Grill!
 </h1>
@@ -32,19 +32,4 @@
     Wij zijn een Studentenvereniging die zich inzet voor mensen die houden van
     bbq. <br /> Opgericht in het jaar 2022 en nog steeds levend!
   </p>
-  <!-- curvy thingy
-    <div class="custom-shape-divider-top-1669586482">
-    <svg
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1200 120"
-      preserveAspectRatio="none"
-    >
-      <path
-        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-        class="shape-fill"
-      />
-    </svg>
-  </div>
--->
 </section>
