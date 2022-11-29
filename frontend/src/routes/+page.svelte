5e7b1ee63f9a32d4b0f38786b5fbdac0e5838c2f
Tom.Burgelman <tom.burgelman@me.com>
2022-11-29 23:43:05 +0100

    fixed grid on home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,7 +8,12 @@
       width="170px"
       alt=""
     />
-    <h1 class="text-3xl font-bold" id="mainname">VADER GRILL</h1>
+    <!--Header text underneath the logo-->
+    <h1 class="text-3xl font-bold headertext">VADER GRILL</h1>
+    <p class="headertext font-semibold">
+      De Studentenvereniging voor mensen die houden van BBQ!
+    </p>
+    <p class="headertext font-semibold">Est. 2022</p>
   </div>
   <!--Creating the 'linten' underneath the picture-->
   <div id="lint">
@@ -16,20 +21,19 @@
     <div class="bg-red-600 -z-20 lint-rood w-full" />
   </div>
 </header>
-<!--Each section is a different block on the page-->
-<section class="m-8 rounded-3xl text-center">
-<h1 class="mx-4 my-8 text-3xl font-bold underline text-center">
-  Welkom bij Vader Grill!
-</h1>
-<p class="m-4 text-center">
-  De Studentenvereniging voor studenten die houden van BBQ!
-</p>
-</section>
 
-<section class="bg-red-900 m-8 rounded-3xl text-center">
-  <h1 class="text-3xl font-bold underline m-4">Over Ons</h1>
-  <p>
-    Wij zijn een Studentenvereniging die zich inzet voor mensen die houden van
-    bbq. <br /> Opgericht in het jaar 2022 en nog steeds levend!
-  </p>
-</section>
+<!-- Using a grid layout for the home page -->
+
+<div class="gap-1 homepagegrid relative">
+  <section class="bg-red-900 m-4 rounded-3xl text-center">
+    <h1 class="text-3xl font-bold underline m-4">Over Ons</h1>
+    <p>
+      Wij zijn een Studentenvereniging die zich inzet voor mensen die houden van
+      bbq. <br /> Opgericht in het jaar 2022 en nog steeds levend!
+    </p>
+  </section>
+  <section class="bg-black m-4 rounded-3xl text-center">
+    <h1 class="text-3xl font-bold underline m-4">Evenementen</h1>
+    <p>Hier zouden dan een paar Evenementen komen.</p>
+  </section>
+</div>
