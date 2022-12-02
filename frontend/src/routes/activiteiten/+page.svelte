2563026e8f5bedb510c8f84aeba5f68083c808e6
Tom Burgelman <tom.burgelman@me.com>
2022-12-02 23:34:12 +0100

    grid system added to activiteiten

--- frontend/src/routes/activiteiten/+page.svelte
+++ frontend/src/routes/activiteiten/+page.svelte
@@ -17,18 +17,18 @@
     ];
 </script>
 
-<h2 class="text-2xl font-bold text-left m-4">Aankomende activiteiten</h2>
-<!-- Main Flex div for all activities-->
-<div class="flex-col ">
+<h2 class="text-2xl font-bold text-left m-4 text-center">Aankomende activiteiten</h2>
+<!-- Main block div for all activities-->
+<div class="block">
     {#each data.activiteiten.items as act}
-        <div class="flex m-4 px-4 py-2 align-center gap-8 justify-start rounded-xl activiteit">
-            <div class="max-w-xs">
+        <div class="grid grid-cols-1 md:grid-cols-3 w-9/12 my-4 mx-auto px-4 py-3 align-middle gap-8 rounded-xl activiteit">
+            <div>
                 <a
                     href={"/activiteiten/" + act.slug}
                     class="hover:text-red-600">{act.titel}</a
                 >
             </div>
-            <div class="flex gap-2 max-w-xs">
+            <div class="flex gap-2">
                 <img src="/date.svg" alt="">
                 <p>
                     {act.start.slice(8, 10)}
