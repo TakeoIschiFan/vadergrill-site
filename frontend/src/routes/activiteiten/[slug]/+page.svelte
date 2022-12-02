a6ff7f8e17a7a5e297be7df9915618e27b7802c1
Tom Burgelman <tom.burgelman@me.com>
2022-12-02 23:52:25 +0100

    very nice

--- frontend/src/routes/activiteiten/[slug]/+page.svelte
+++ frontend/src/routes/activiteiten/[slug]/+page.svelte
@@ -3,8 +3,8 @@
     export let data;
 </script>
 
- <h1>
-    dit is eventement slug {data.activiteit.titel} 
+ <h1 class="text-3xl font-bold underline text-center m-4">
+  {data.activiteit.titel} 
 </h1>
 
 <p>{data.activiteit.beschrijving}</p>
