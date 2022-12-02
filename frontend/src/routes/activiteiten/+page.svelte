3c309b0ed46ea8e0f284fd39772ddd64f6ec125f
Tom.Burgelman <tom.burgelman@me.com>
2022-12-02 16:36:47 +0100

    added svg's

--- frontend/src/routes/activiteiten/+page.svelte
+++ frontend/src/routes/activiteiten/+page.svelte
@@ -1,18 +1,52 @@
-
-<h1 class="text-3xl font-bold underline text-center">Evenementen</h1>
-
 <script>
+    export let data;
 
-export let data;
-
+    export let maanden = [
+        "Januari",
+        "Februari",
+        "Maart",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Augustus",
+        "September",
+        "Oktober",
+        "November",
+        "December",
+    ];
 </script>
 
-<h2>De volgende activiteiten zijn:</h2>
-<ul>
+<h2 class="text-2xl font-bold text-left m-4">Aankomende activiteiten</h2>
+<!-- Main Flex div for all activities-->
+<div class="flex-col ">
     {#each data.activiteiten.items as act}
-    <li>
-        <a href={"/activiteiten/" + act.slug}>{act.titel}</a> 
-        <p> </p>
-    </li>
-{/each}
-</ul>
+        <div class="flex m-4 px-4 py-2 align-center gap-8 justify-start rounded-xl activiteit">
+            <div>
+                <a
+                    href={"/activiteiten/" + act.slug}
+                    class="hover:underline">{act.titel}</a
+                >
+            </div>
+            <div class="flex gap-2">
+                <img src="/date.svg" alt="" class="h-7 w-7">
+                <p>
+                    {act.start.slice(8, 10)}
+                    {maanden[act.start.slice(5, 7)]}
+                    {act.start.slice(0, 4)}
+                </p>
+            </div>
+            <div class="flex gap-2">
+                <img src="/clock.svg" alt="" class="w-6 h-6">
+                <p>{act.start.slice(11, 16)}</p>
+            </div>
+        </div>
+    {/each}
+</div>
+
+<!--Started using style tags for better oversight-->
+<style>
+    .activiteit {
+        background-color: rgb(13,17,23);
+    }
+</style>
