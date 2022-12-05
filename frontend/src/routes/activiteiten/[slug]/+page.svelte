4f0f66bb5f44e9c95de3911cbce06192ad0d0645
Tom.Burgelman <tom.burgelman@me.com>
2022-12-05 20:15:21 +0100

    smooth backlink

--- frontend/src/routes/activiteiten/[slug]/+page.svelte
+++ frontend/src/routes/activiteiten/[slug]/+page.svelte
@@ -5,44 +5,44 @@
 
 <!--Backwards arrow-->
 <div class="m-4">
-  <a href="/activiteiten">
-    <svg version="1.1" id="arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 477.175 477.175"  style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve" width="40px" height="40px">
-<g>
-	<path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
-		c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z" fill="currentcolor"/>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-</svg></a
+  <a href="/activiteiten" class="flex items-center backlink">
+    <svg
+      version="1.1"
+      id="arrow"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 477.175 477.175"
+      style="enable-background:new 0 0 477.175 477.175;"
+      xml:space="preserve"
+      width="40px"
+      height="40px"
+    >
+      <g>
+        <path
+          d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
+		c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"
+          fill="currentcolor"
+        />
+      </g>
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+    </svg>
+    <p class="backtext">Terug</p></a
   >
 </div>
 
@@ -52,8 +52,8 @@
 
 <!--Beschrijving div-->
 <div class="m-4 divbox rounded-2xl">
-<h2 class="my-4 text-2xl font-bold">Beschrijving</h2>
-<p class="mb-4">{data.activiteit.beschrijving}</p>
+  <h2 class="my-4 text-2xl font-bold">Beschrijving</h2>
+  <p class="mb-4">{data.activiteit.beschrijving}</p>
 </div>
 
 <style>
@@ -61,10 +61,25 @@
     color: white;
   }
 
-  #arrow:hover {
+  .backtext {
+    font: bold;
+    color: white;
+    transform: translate(-30px, 0px);
+    opacity: 0%;
+  }
+
+  .backlink:hover .backtext{
+    transition: 400ms;
+    color: red;
+    opacity: 100%;
+    transform: translate(0, 0px);
+  }
+
+  .backlink:hover #arrow {
     color: rgb(220, 38, 38);
     transition: 400ms;
   }
+
 
   .divbox {
     background-color: rgb(13, 17, 23);
