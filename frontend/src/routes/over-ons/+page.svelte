a2da9903281504ab3453a3be02a6bfc7f6e5f793
Tom.Burgelman <tom.burgelman@me.com>
2022-12-13 00:43:16 +0100

    praesidium fixed

--- frontend/src/routes/over-ons/+page.svelte
+++ frontend/src/routes/over-ons/+page.svelte
@@ -1,8 +1,9 @@
 <script>
   import PraesidiumCard from "$lib/components/praesidiumCard.svelte";
   import { each } from "svelte/internal";
-
+  import PocketBase from 'pocketbase';
   export let data;
+  export const pb = new PocketBase('http://127.0.0.1:8090');
 </script>
 
 <div class="flex flex-col gap-2 items-center mt-4">
@@ -13,33 +14,22 @@
 <div class="relative top-12 mx-2 mb-20">
   <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ml-4 mr-4">
     {#each data.bestuur.items as lid}
-    <p>{lid.foto}</p>
       <div class="relative z-10 bg-black rounded-lg right-0.5 praesidiumkaart">
-        <div class="grid grid-cols-2 gap-1">
-          <!--
-          {#if data.bestuurfotos[lid.nummer].slice(-3) == "jpg"}
+        <div class="grid grid-cols-2 gap-1 h-80">
           <img
-            src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/16:9/w_2123,h_1194,c_limit/phonepicutres-TA.jpg"
-            alt=""
+            src="{pb.getFileUrl(lid, lid.foto)}"
+            alt="{lid.naam}"
             draggable="false"
-            class="absolute box-border inset-0 p-0 border-none m-auto block  w-0 h-0 min-w-full max-w-full min-h-full max-h-full shadow-lg md:rounded-t-lg object-cover"
+            class="absolute -z-10 border-none w-full h-80 rounded-lg object-cover"
           />
-        {:else}
-          <img
-            src="/testfoto.jpg"
-            alt="bakcground"
-            draggable="false"
-            class="absolute box-border inset-0 p-0 border-none m-auto block  w-0 h-0 min-w-full max-w-full min-h-full max-h-full shadow-lg md:rounded-t-lg object-cover"
-          />
-        {/if} -->
           <div class="ml-5 mt-5">
-            <h1 class="relative w-full flex text-2xl font-semibold text-white">
+            <h1 class="relative w-full flex text-2xl font-semibold text-white headertext">
               {lid.bijnaam}
             </h1>
-            <h3 class="relative w-full flex text-m font-light text-white">
+            <h3 class="relative w-full flex text-m font-light text-white headertext">
               {lid.functie}
             </h3>
-            <h2 class="relative w-full flex text-xl font-light text-white">
+            <h2 class="relative w-full flex text-xl font-light text-white headertext">
               {lid.naam}
             </h2>
           </div>
