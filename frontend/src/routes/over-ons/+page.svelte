e1f647b07676ebdd28477700215e746034b78dcb
Tom.Burgelman <tom.burgelman@me.com>
2022-12-12 12:47:22 +0100

    testing

--- frontend/src/routes/over-ons/+page.svelte
+++ frontend/src/routes/over-ons/+page.svelte
@@ -10,12 +10,33 @@
 <!-- Grid use for better layout-->
 <div class="relative top-12 mx-2 mb-20">
   <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ml-4 mr-4">
-    {#each data.bestuur.items as lid}
-      <PraesidiumCard
-        functie={lid.bijnaam}
-        functie2={lid.functie}
-        naam={lid.naam}
-      />
-    {/each}
+      <div class="relative z-10 bg-black rounded-lg right-0.5 praesidiumkaart">
+        <div class="grid grid-cols-2 gap-1">
+          <img
+            src="/testfoto.jpg"
+            alt=""
+            class="relative z-10 inset-0 w-full h-full object-cover rounded-t-lg rounded-bl-lg bg-red-100"
+            loading="lazy"
+          />
+          <div class="ml-5 mt-5">
+            <h1 class="relative w-full flex text-2xl font-semibold text-white">
+              je mama
+            </h1>
+            <h3 class="relative w-full flex text-m font-light text-white">
+              je mama
+            </h3>
+            <h2 class="relative w-full flex text-xl font-light text-white">
+              je mama
+            </h2>
+          </div>
+        </div>
+      </div>
   </div>
 </div>
+
+<style>
+  .praesidiumkaart:hover {
+    transform: scale(1.05);
+    transition: 400ms ease-in-out;
+  }
+</style>
