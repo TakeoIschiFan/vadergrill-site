c0b25e1cd4fbfd089e128895aeab51e90fb3f169
Tom.Burgelman <tom.burgelman@me.com>
2022-12-13 11:47:18 +0100

    woordenboek page

---
+++ frontend/src/routes/woordenboek/+page.server.ts
@@ -0,0 +1,11 @@
+import { serializeNonPOJO } from "$lib/utils";
+import type { PageServerLoad } from "./$types";
+
+export async function load({ locals, params }): PageServerLoad {
+
+    const woorden = serializeNonPOJO(await locals.pb.collection("woordenboek").getFullList(200, { sort: "-woord" }));
+    console.log(woorden)
+    return {
+        woordenboek: woorden
+    };
+};
---
+++ frontend/src/routes/woordenboek/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    export let data;
+</script>
+
+<div class="maxwidth mx-auto">
+    {#each data.woordenboek.items as woord}
+        <div class="flex flex-col">
+            <p>
+                {woord.woord}
+            </p>
+            <p>
+                {woord.beschrijving}
+            </p>
+        </div>
+    {/each}
+</div>
