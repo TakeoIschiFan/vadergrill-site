fbc6ae77b81659297ad44fa2377685dfdc2fbf3f
Tom.Burgelman <tom.burgelman@me.com>
2022-12-13 12:42:55 +0100

    updated register page design

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,39 +1,69 @@
-<div class="flex flex-col items-center h-full w-full">
-	<h2 class="mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
+<div
+	class="flex flex-col items-center h-full w-full maxwidth min-h-screen mt-4"
+>
+	<h2
+		class="mt-2 text-center text-3xl font-bold tracking-tight text-base-content"
+	>
 		Word nu lid!
 	</h2>
 	<p class="text-center mt-1">
-		Of <a href="/login" class="text-primary font-medium hover:cursor-pointer hover:underline"
+		Of <a
+			href="/login"
+			class="text-primary font-medium hover:cursor-pointer underline hover:text-red-600"
 			>login</a
 		> als je al een account hebt.
 	</p>
-	<form action="?/register" method="POST" class="flex flex-col items-center space-y-2 w-full pt-4">
+	<form
+		action="?/register"
+		method="POST"
+		class="flex flex-col items-center space-y-2 w-full pt-4 text-center gap-2"
+	>
 		<div class="form-control w-full max-w-md">
-			<label for="username" class="label font-medium pb-1">
-				<span class="label-text">Naam</span>
-			</label>
-			<input type="text" name="username" class="input input-bordered w-full max-w-md" />
+			<input
+				type="text"
+				name="username"
+				class="input input-bordered w-2/3 max-w-md text-black rounded-lg p-1"
+				placeholder="naam"
+			/>
 		</div>
 		<div class="form-control w-full max-w-md">
-			<label for="email" class="label font-medium pb-1">
-				<span class="label-text">Email</span>
-			</label>
-			<input type="email" name="email" class="input input-bordered w-full max-w-md" />
+			<input
+				type="email"
+				name="email"
+				class="input input-bordered w-2/3 max-w-md text-black rounded-lg p-1"
+				placeholder="e-mail@example.com"
+			/>
 		</div>
 		<div class="form-control w-full max-w-md">
-			<label for="password" class="label font-medium pb-1">
-				<span class="label-text">Wachtwoord</span>
-			</label>
-			<input type="password" name="password" class="input input-bordered w-full max-w-md" />
+			<input
+				type="password"
+				name="password"
+				class="input input-bordered w-2/3 max-w-md text-black rounded-lg p-1"
+				placeholder="password"
+			/>
 		</div>
 		<div class="form-control w-full max-w-md">
-			<label for="passwordConfirm" class="label font-medium pb-1">
-				<span class="label-text">Bevestig Wachtwoord</span>
-			</label>
-			<input type="password" name="passwordConfirm" class="input input-bordered w-full max-w-md" />
+			<input
+				type="password"
+				name="passwordConfirm"
+				class="input input-bordered w-2/3 max-w-md text-black rounded-lg p-1"
+				placeholder="Comfirm password"
+			/>
 		</div>
 		<div class="w-full max-w-md pt-2">
-			<button type="submit" class="btn btn-primary w-full">Registreer</button>
+			<button type="submit" class="bg-red-600 w-36 hover:opacity-80 loginbtn rounded-lg font-bold text-xl shadow-lg p-1"
+				>Registreer</button
+			>
 		</div>
 	</form>
 </div>
+
+<style>
+	.loginbtn {
+		transition: 400ms;
+	}
+	.loginbtn:hover {
+		transform: scale(1.05);
+	}
+</style>
+
