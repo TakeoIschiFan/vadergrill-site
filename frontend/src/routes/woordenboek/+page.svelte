e4841398be64f6b6c01784db02cb16fbc4c1bedc
Tom Burgelman <tom.burgelman@me.com>
2022-12-15 00:17:28 +0100

    woordenboek updated

--- frontend/src/routes/woordenboek/+page.svelte
+++ frontend/src/routes/woordenboek/+page.svelte
@@ -1,13 +1,21 @@
 <script>
-
-    export let data;
+  export let data;
 </script>
 
-<div class="maxwidth mx-auto">
-    {#each data.woordenboek.items as woord}
-        <div class="flex flex-row gap-8">
-            <p>{woord.woord}</p>
-            <p>{woord.beschrijving}</p>
+<div class="maxwidth mx-auto min-h-screen">
+  {#each data.woordenboek.items as woord}
+    <div class="flex flex-col">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="pl-4">
+          <p class="m-4 text-3xl font-bold underline">{woord.woord}</p>
         </div>
-    {/each}
+        <div class="mt-6">
+          <p class="m-4 text-2xl font-bold">{woord.beschrijving}</p>
+        </div>
+      </div>
+      <div
+        class="h-1 w-11/12 top-0 bg-gradient-to-r from-red-600 to-red-700 self-center rounded-lg"
+      />
+    </div>
+  {/each}
 </div>
